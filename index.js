2bdb67edf7c2cf6c8429c4c365d602842d6f85aa
daeyoung63 <jdy5799@gmail.com>
2025-04-10 02:01:44 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import TripSquareApp from "./TripSquareApp";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <TripSquareApp />
+  </React.StrictMode>
+);
